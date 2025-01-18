17424f57f23565a2171205f934f0fad2eca6e482
thitina begashaw <thitinabegashawtilahun@gmail.com>
2025-01-19 00:04:36 +0300

    Add Cloudinary

--- backend/controller/MenuController.js
+++ backend/controller/MenuController.js
@@ -1,1 +1,17 @@
-import express from "express";
+import menu from "../model/MenuSchema"
+
+const createMenu = async (req, res) => {
+  const { Title, Description, Price , imageUrl } = req.body;
+  try{
+    const Menu = await menu.create({Title , Description, Price ,imageUrl})
+    res.status(200).json(Menu)
+  }
+
+    catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Failed to add menu",
+      });
+  }
+  
+  }
