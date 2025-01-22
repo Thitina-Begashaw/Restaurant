d63be959637206c181bfcc6cbf18786bb09d3dc5
thitina begashaw <thitinabegashawtilahun@gmail.com>
2025-01-22 22:41:51 +0300

    Add Frontend Header

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import forklogo from "./assets/fork3.jpg";
+import Rlogo from "./assets/Logo.jpg";
 export default function Header(){
 
   return(
-    <div className="">
-      <div> 
-        <img src= {forklogo} />
-        <h3 className="text-3xl font-bold underline">Restaurant</h3>
+    <div className="bg-[url('./assets/Back3.jpg')] bg-cover bg-center min-h-screen">
+      <div className="flex flex-row gap-12 bg-black h-28"> 
+        <img className="absolute left-10 top-0 w-28 h-28  p-2 rounded-full" src= {Rlogo} />
+        <h3 className=" text-white text-5xl ml-36 mt-6">Restaurant</h3>
+        <div className="flex flex-row gap-12 text-white items-centers ml-72 mt-10">
         <p>HOME</p>
         <p>ABOUT</p>
         <p>SERVICE</p>
         <p>MENU</p>
         <p>CONTACT</p>
-        <button>BOOK A TABLE</button>
+        <button className="flex ">BOOK A TABLE</button>
+        </div>
       </div>
-
     </div>
+  
   );
 }
