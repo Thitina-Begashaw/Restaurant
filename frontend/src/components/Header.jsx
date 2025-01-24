0e664fe48bb0e3c6e882951270c9e893836f07e5
thitina begashaw <thitinabegashawtilahun@gmail.com>
2025-01-24 23:25:59 +0300

    Add update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,11 +24,18 @@ export default function Header() {
     };
   }, []);
 
+  const handleHomeClick = () => {
+    // Animate scrolling to top
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })}
+
   return (
     <div className="bg-[url('./assets/Bg8.jpg')] bg-cover bg-center h-[150vh]">
       <div
         className={`flex flex-row gap-12 font-bold h-28 fixed z-10 ${
-          isScrolled ? "bg-black w-lvw h-24 transition-colors duration-300" : "bg-transparent"
+          isScrolled ? "bg-black w-lvw h-20 transition-colors duration-300" : "bg-transparent"
         }`}
       >
         <img
@@ -36,11 +43,11 @@ export default function Header() {
           src={Rlogo}
           alt="Restaurant Logo"
         />
-        <h3 className="cursor-pointer hover:text-orange-400 text-5xl ml-40 mt-6 text-orange-400 font-extrabold">
+        <h3 className={`cursor-pointer hover:text-orange-400 text-5xl ml-40 mt-6 text-orange-400 font-extrabold ${isScrolled ? "mt-3" : "mt-6"}`}>
           Restaurant
         </h3>
         <div className="flex flex-row gap-10 text-white items-center ml-52 text-lg">
-          <p className="cursor-pointer hover:text-orange-400 transition duration-300">HOME</p>
+          <p className="cursor-pointer hover:text-orange-400 transition duration-300" onClick={handleHomeClick}>HOME</p>
           <p className="cursor-pointer hover:text-orange-400 transition duration-300">ABOUT</p>
           <p className="cursor-pointer hover:text-orange-400 transition duration-300">SERVICE</p>
           <p className="cursor-pointer hover:text-orange-400 transition duration-300">MENU</p>
