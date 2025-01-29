ba7c49f729a7db2b23b171e54112f521744822cc
thitina begashaw <thitinabegashawtilahun@gmail.com>
2025-01-29 23:03:40 +0300

    Update Header of the component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -35,17 +35,25 @@ export default function Header() {
     <div className="bg-[url('./assets/Bg8.jpg')] bg-cover bg-center h-[150vh]">
       <div
         className={`flex flex-row gap-12 font-bold h-28 fixed z-10 ${
-          isScrolled ? "bg-black w-lvw h-20 transition-colors duration-300" : "bg-transparent"
+          isScrolled ? "bg-slate-800  w-lvw h-20 transition-colors duration-300" : "bg-transparent"
         }`}
       >
         <img
-          className="absolute left-10 top-0 w-24 h-24 p-2 rounded-full"
+         className={`absolute left-10 top-0 w-24 h-24 p-2 rounded-full ${isScrolled ? "h-20 w-20" : "h-24"}`}
+
           src={Rlogo}
           alt="Restaurant Logo"
         />
-        <h3 className={`cursor-pointer hover:text-orange-400 text-5xl ml-40 mt-6 text-orange-400 font-extrabold ${isScrolled ? "mt-3" : "mt-6"}`}>
+        <h3 className={` text-5xl ml-40  text-orange-500 font-extrabold  ${isScrolled ? "mt-3 h-20 text-3xl" : "mt-6 h-auto"}`}>
           Restaurant
         </h3>
+
+        {/* <h3 className={`cursor-pointer hover:text-orange-400 text-5xl ml-40 font-extrabold transition-all duration-300 ${
+  isScrolled ? "mt-3 h-20 text-3xl" : "mt-6 h-auto"
+}`}>
+  Restaurant
+</h3> */}
+
         <div className="flex flex-row gap-10 text-white items-center ml-52 text-lg">
           <p className="cursor-pointer hover:text-orange-400 transition duration-300" onClick={handleHomeClick}>HOME</p>
           <p className="cursor-pointer hover:text-orange-400 transition duration-300">ABOUT</p>
